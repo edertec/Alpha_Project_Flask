08fd6b97cf8a59209f563b7b578a981cf1691d0d
Ederson Almeida <edertec@msn.com>
2024-05-26 16:22:41 -0300

    detalhes dos alunos

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 from app.controllers import alunos
 
@@ -9,3 +9,8 @@ def home():
 @app.route('/alunos')
 def listar_alunos():
     return render_template('listar_alunos.html', alunos=alunos)
+
+@app.route('/alunos/<int:aluno_id>')
+def detalhes_aluno(aluno_id):
+    aluno = alunos[aluno_id]
+    return render_template('detalhes_aluno.html', aluno=aluno)
